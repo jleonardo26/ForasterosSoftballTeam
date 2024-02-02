e6aeac32da13c60dd6e4f6434d851c141fa837ea
jleonardo26 <ingjoelleonardo26@gmail.com>
2024-02-01 20:40:39 -0400

    forasteros 1st commit

---
+++ playercard.js
@@ -0,0 +1,203 @@
+
+  //estadisticas, player card
+
+
+  // Alexander Peña
+  function VerTarjetaJugadorAlexanderPena(){
+    document.getElementById("PlayerCardBateadorAlexanderPena").style.display="block";
+}
+
+  function CerrarTarjetaJugadorAlexanderPena(){
+      document.getElementById("PlayerCardBateadorAlexanderPena").style.display="none";
+  }
+
+  //Andres Fulcar
+  function VerTarjetaJugadorAndresFulcar(){
+    document.getElementById("PlayerCardBateadorAndresFulcar").style.display="block";
+}
+
+  function CerrarTarjetaJugadorAndresFulcar(){
+      document.getElementById("PlayerCardBateadorAndresFulcar").style.display="none";
+  }
+
+  //Angel Ogando
+  function VerTarjetaJugadorAngelOgando(){
+    document.getElementById("PlayerCardBateadorAngelOgando").style.display="block";
+}
+
+  function CerrarTarjetaJugadorAngelOgando(){
+      document.getElementById("PlayerCardBateadorAngelOgando").style.display="none";
+  }
+
+  //Baldwin Coplin
+  function VerTarjetaJugadorBaldwinCoplin(){
+    document.getElementById("PlayerCardBateadorBaldwinCoplin").style.display="block";
+}
+
+  function CerrarTarjetaJugadorBaldwinCoplin(){
+      document.getElementById("PlayerCardBateadorBaldwinCoplin").style.display="none";
+  }
+
+  //Bryan Terrero
+  function VerTarjetaJugadorBryanTerrero(){
+    document.getElementById("PlayerCardBateadorBryanTerrero").style.display="block";
+}
+
+  function CerrarTarjetaJugadorBryanTerrero(){
+      document.getElementById("PlayerCardBateadorBryanTerrero").style.display="none";
+  }
+
+   //Felix Ramirez
+   function VerTarjetaJugadorFelixRamirez(){
+    document.getElementById("PlayerCardBateadorFelixRamirez").style.display="block";
+}
+
+  function CerrarTarjetaJugadorFelixRamirez(){
+      document.getElementById("PlayerCardBateadorFelixRamirez").style.display="none";
+  }
+
+   //Francisco Montero
+   function VerTarjetaJugadorFranciscoMontero(){
+    document.getElementById("PlayerCardBateadorFranciscoMontero").style.display="block";
+}
+
+  function CerrarTarjetaJugadorFranciscoMontero(){
+      document.getElementById("PlayerCardBateadorFranciscoMontero").style.display="none";
+  }
+
+  //Jeffry Pena
+  function VerTarjetaJugadorJeffryPena(){
+    document.getElementById("PlayerCardBateadorJeffryPena").style.display="block";
+}
+
+  function CerrarTarjetaJugadorJeffryPena(){
+      document.getElementById("PlayerCardBateadorJeffryPena").style.display="none";
+  }
+
+  //Joel Leonardo
+
+  function VerTarjetaJugadorJoelLeonardo(){
+    document.getElementById("PlayerCardBateadorJoelLeonardo").style.display="block";
+}
+
+  function CerrarTarjetaJugadorJoelLeonardo(){
+      document.getElementById("PlayerCardBateadorJoelLeonardo").style.display="none";
+  }
+
+  //Johan Ramirez
+  function VerTarjetaJugadorJohanRamirez(){
+    document.getElementById("PlayerCardBateadorJohanRamirez").style.display="block";
+}
+
+  function CerrarTarjetaJugadorJohanRamirez(){
+      document.getElementById("PlayerCardBateadorJohanRamirez").style.display="none";
+  }
+
+  //Malvin Marcano
+  function VerTarjetaJugadorMalvinMarcano(){
+    document.getElementById("PlayerCardBateadorMalvinMarcano").style.display="block";
+}
+
+  function CerrarTarjetaJugadorMalvinMarcano(){
+      document.getElementById("PlayerCardBateadorMalvinMarcano").style.display="none";
+  }
+
+  //Ramon ¨RAY¨ Rosario
+  function VerTarjetaJugadorRamonRosario(){
+    document.getElementById("PlayerCardBateadorRamonRosario").style.display="block";
+}
+
+  function CerrarTarjetaJugadorRamonRosario(){
+      document.getElementById("PlayerCardBateadorRamonRosario").style.display="none";
+  }
+
+  //Jose Terrero
+  function VerTarjetaJugadorJoseTerrero(){
+    document.getElementById("PlayerCardBateadorJoseTerrero").style.display="block";
+}
+
+  function CerrarTarjetaJugadorJoseTerrero(){
+      document.getElementById("PlayerCardBateadorJoseTerrero").style.display="none";
+  }
+
+  //Victor Feliz
+  function VerTarjetaJugadorVictorFeliz(){
+    document.getElementById("PlayerCardBateadorVictorFeliz").style.display="block";
+}
+
+  function CerrarTarjetaJugadorVictorFeliz(){
+      document.getElementById("PlayerCardBateadorVictorFeliz").style.display="none";
+  }
+
+  //Wilmer Piña
+  function VerTarjetaJugadorWilmerPina(){
+    document.getElementById("PlayerCardBateadorWilmerPina").style.display="block";
+}
+
+  function CerrarTarjetaJugadorWilmerPina(){
+      document.getElementById("PlayerCardBateadorWilmerPina").style.display="none";
+  }
+
+  //Yefry Perez
+  function VerTarjetaJugadorYefryPerez(){
+    document.getElementById("PlayerCardBateadorYefryPerez").style.display="block";
+}
+
+  function CerrarTarjetaJugadorYefryPerez(){
+      document.getElementById("PlayerCardBateadorYefryPerez").style.display="none";
+  }
+
+  //Yordy Feliz "VENTURA"
+  function VerTarjetaJugadorYordyFeliz(){
+    document.getElementById("PlayerCardBateadorYordyFeliz").style.display="block";
+}
+
+  function CerrarTarjetaJugadorYordyFeliz(){
+      document.getElementById("PlayerCardBateadorYordyFeliz").style.display="none";
+  }
+
+  //Daniel Pineda
+  function VerTarjetaJugadorDanielPineda(){
+    document.getElementById("PlayerCardBateadorDanielPineda").style.display="block";
+}
+
+  function CerrarTarjetaJugadorDanielPineda(){
+      document.getElementById("PlayerCardBateadorDanielPineda").style.display="none";
+  }
+
+
+
+  //ESTADISTICAS DE PITCHERS
+
+  // //Daniel Pineda Pitcher
+  // function verDanielPinedaPitcher() {
+  //   document.getElementById("cardDanielPinedaPitcher").style.display="block";
+  // }
+
+  // function cerrarDanielPinedaPitcher(){
+  //   document.getElementById("cardDanielPinedaPitcher").style.display="none";
+  // }
+
+  // //Alexander "BRAYAN" Peña Pitcher
+  // function verAlexanderPenaPitcher() {
+  //   document.getElementById("cardAlexanderPenaPitcher").style.display="block";
+  // }
+
+  // function cerrarAlexanderPenaPitcher(){
+  //   document.getElementById("cardAlexanderPenaPitcher").style.display="none";
+  // }
+
+  // //Joel Leonardo Pitcher
+  // function verJoelLeonardoPitcher() {
+  //   document.getElementById("cardJoelLeonardoPitcher").style.display="block";
+  // }
+
+  // function cerrarJoelLeonardoPitcher(){
+  //   document.getElementById("cardJoelLeonardoPitcher").style.display="none";
+  // }
+
+
+
+
+  
+
