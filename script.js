baa90dbcee48233ac10f2f3817ad484f2a0b42cf
jleonardo26 <ingjoelleonardo26@gmail.com>
2024-03-15 19:06:40 -0400

    actualizacion 15 marzo

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 
 //CODIGO dropdown de barra de navegacion
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdowns = document.querySelectorAll('.btn-inicio');
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdowns = document.querySelectorAll('.btn-inicio');
   
-    dropdowns.forEach(function (dropdown) {
-      dropdown.addEventListener('click', function () {
-        dropdown.parentElement.querySelector('.dropdown').classList.toggle('show');
-      });
-    });
+//     dropdowns.forEach(function (dropdown) {
+//       dropdown.addEventListener('click', function () {
+//         dropdown.parentElement.querySelector('.dropdown').classList.toggle('show');
+//       });
+//     });
   
-    window.addEventListener('click', function (event) {
-      if (!event.target.matches('.btn-inicio')) {
-        const dropdowns = document.querySelectorAll('.dropdown');
-        dropdowns.forEach(function (dropdown) {
-          if (dropdown.classList.contains('show')) {
-            dropdown.classList.remove('show');
-          }
-        });
-      }
-    });
-  });
+//     window.addEventListener('click', function (event) {
+//       if (!event.target.matches('.btn-inicio')) {
+//         const dropdowns = document.querySelectorAll('.dropdown');
+//         dropdowns.forEach(function (dropdown) {
+//           if (dropdown.classList.contains('show')) {
+//             dropdown.classList.remove('show');
+//           }
+//         });
+//       }
+//     });
+//   });
 
 
 
